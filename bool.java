f4c22265ff5814b6b4fe6b2d988e0977220ac4de
MANIKANTA3224 <153202534+MANIKANTA3224@users.noreply.github.com>
2023-12-07 19:47:05 +0530

    Add files via upload

---
+++ bool.java
@@ -0,0 +1,14 @@
+public class bool {
+    public static void main(String[] args) {
+        int myage=25;
+        int voteneed=18;
+        if(myage>=voteneed){
+            System.out.println("person is eligible for vote");
+        }
+        else {
+            System.out.println("person is not eligible for vote");
+        }
+
+        }
+    }
+
